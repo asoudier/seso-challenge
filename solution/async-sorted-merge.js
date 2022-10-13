d22b772c624d61b24f5334a36947ca888ab0bb1c
Alex Soudier <alex.soudier@onedrop.today>
2022-10-13 13:15:20 -0500

    Update async-sorted-merge.js

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,32 @@
 "use strict";
-
 // Print all entries, across all of the *async* sources, in chronological order.
 
+// Basically the same idea here except async
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
+    const logEntriesPromise = [];
+
+    let i = 0;
+    while (i < logSources.length) {
+      logEntriesPromise.push(logSources[i].popAsync());
+      i++;
+    }
+
+    Promise.all(logEntriesPromise).then((results) => {
+      const sortedLog = results
+        .map((item) => [+new Date(item.date), item])
+        .sort(([a], [b]) => a - b)
+        .map(([, item]) => item);
+
+      i = 0;
+      while (i < logSources.length) {
+        printer.print(sortedLog[i]);
+        i++;
+      }
+
+      printer.done();
+    });
+
     resolve(console.log("Async sort complete."));
   });
 };
