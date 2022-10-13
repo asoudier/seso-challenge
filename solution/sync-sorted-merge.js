2edeb768c4f5dd3c580b4a8b721af88461011987
Alex Soudier <alex.soudier@onedrop.today>
2022-10-13 13:15:41 -0500

    fix logic for sync

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,32 +1,32 @@
 "use strict";
-// Print all entries, across all of the *async* sources, in chronological order.
 
-// Basically the same idea here except async
+// Print all entries, across all of the sources, in chronological order.
 module.exports = (logSources, printer) => {
-  return new Promise((resolve) => {
-    const logEntriesPromise = [];
+  let log = [];
 
-    let i = 0;
-    while (i < logSources.length) {
-      logEntriesPromise.push(logSources[i].popAsync());
-      i++;
-    }
+  // for data manipulation used while loops they are the fastest in javascript
+  // https://stackoverflow.com/questions/5349425/whats-the-fastest-way-to-loop-through-an-array-in-javascript
+  let i = 0;
+  while (i < logSources.length) {
+    log.push(logSources[i].pop());
+    i++;
+  }
 
-    Promise.all(logEntriesPromise).then((results) => {
-      const sortedLog = results
-        .map((item) => [+new Date(item.date), item])
-        .sort(([a], [b]) => a - b)
-        .map(([, item]) => item);
+  // used the schwartzian transformations for faster sorting https://en.wikipedia.org/wiki/Schwartzian_transform
+  const sortedLog = log
+    .map((item) => [+new Date(item.date), item])
+    .sort(([a], [b]) => a - b)
+    .map(([, item]) => item);
 
-      i = 0;
-      while (i < logSources.length) {
-        printer.print(sortedLog[i]);
-        i++;
-      }
+  // log.sort((a, b) => a.date - b.date);
 
-      printer.done();
-    });
+  i = 0;
+  while (i < sortedLog.length) {
+    printer.print(sortedLog[i]);
+    i++;
+  }
 
-    resolve(console.log("Async sort complete."));
-  });
+  printer.done();
+
+  return console.log("Sync sort complete.");
 };
